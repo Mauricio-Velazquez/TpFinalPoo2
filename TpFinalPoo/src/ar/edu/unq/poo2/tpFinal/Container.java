5b808175313821c90928e3f13cdfc3b1b85cc434
ivanros02 <ivanrosendo1102@gmail.com>
2023-11-08 17:40:05 -0300

    clase Container

--- TpFinalPoo/src/ar/edu/unq/poo2/tpFinal/Container.java
+++ TpFinalPoo/src/ar/edu/unq/poo2/tpFinal/Container.java
@@ -1,5 +1,59 @@
 package ar.edu.unq.poo2.tpFinal;
 
 public class Container {
+	private String tipo; //posible state
+	private double ancho;
+	private double largo;
+	private double altura;
+	private double pesoTotal;
+
+	// Constructor
+	public Container(String tipo, double ancho, double largo, double altura, double pesoTotal) {
+		this.tipo = tipo;
+	    this.ancho = ancho;
+	    this.largo = largo;
+	    this.altura = altura;
+	    this.pesoTotal = pesoTotal;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public double getAncho() {
+		return ancho;
+	}
+
+	public void setAncho(double ancho) {
+		this.ancho = ancho;
+	}
+
+	public double getLargo() {
+		return largo;
+	}
+
+	public void setLargo(double largo) {
+		this.largo = largo;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double getPesoTotal() {
+		return pesoTotal;
+	}
+
+	public void setPesoTotal(double pesoTotal) {
+		this.pesoTotal = pesoTotal;
+	}
 
 }
