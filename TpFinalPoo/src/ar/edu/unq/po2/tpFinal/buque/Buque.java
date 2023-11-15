fd3f691d5db37c0789b93fadc8bca5b45b73f0ff
ivanros02 <ivanrosendo1102@gmail.com>
2023-11-15 16:46:22 -0300

    update buque

--- TpFinalPoo/src/ar/edu/unq/po2/tpFinal/buque/Buque.java
+++ TpFinalPoo/src/ar/edu/unq/po2/tpFinal/buque/Buque.java
@@ -1,5 +1,17 @@
 package ar.edu.unq.po2.tpFinal.buque;
+import ar.edu.unq.po2.tpFinal.container.Container;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Buque {
-
+	int identificador;
+	//idea tipo Par posicionGPSRespecto;
+	FaseDeBuque estadoActual;
+	List <Container> containersAsociados;
+	
+	public Buque(int identificador) {
+		this.identificador = identificador;
+		this.estadoActual = new FaseDeBuqueOutbound();
+		this.containersAsociados = new ArrayList<Container>();
+	}
 }
---
+++ TpFinalPoo/src/ar/edu/unq/po2/tpFinal/buque/FaseDeBuqueWorking.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.po2.tpFinal.buque;
+
+public class FaseDeBuqueWorking implements FaseDeBuque{
+
+}
