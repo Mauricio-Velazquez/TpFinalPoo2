493321a7191f3c9ff2d12457ec4aefc680ad3c26
Mauricio-Velazquez <mauriciovelazquez211@gmail.com>
2023-12-01 01:30:06 -0300

    Test de filtro completado

--- TpFinalPoo/test/ar/edu/unq/po2/tpFinal/terminalGestionada/TerminalGestionadaFiltroTestCase.java
+++ TpFinalPoo/test/ar/edu/unq/po2/tpFinal/terminalGestionada/TerminalGestionadaFiltroTestCase.java
@@ -5,6 +5,7 @@ import ar.edu.unq.po2.tpFinal.filtro.Filtro;
 import ar.edu.unq.po2.tpFinal.filtro.FiltroAnd;
 import ar.edu.unq.po2.tpFinal.filtro.FiltroFechaDeLlegada;
 import ar.edu.unq.po2.tpFinal.filtro.FiltroFechaDeSalida;
+import ar.edu.unq.po2.tpFinal.filtro.FiltroOr;
 import ar.edu.unq.po2.tpFinal.naviera.Circuito;
 import ar.edu.unq.po2.tpFinal.naviera.EstrategiaMenorCosto;
 import ar.edu.unq.po2.tpFinal.naviera.Naviera;
@@ -71,10 +72,24 @@ class TerminalGestionadaFiltroTestCase {
 	 public void testFiltroAnd() {
         FiltroFechaDeLlegada filtro1 = new FiltroFechaDeLlegada(LocalDate.of(2023, 12, 1));
         FiltroFechaDeSalida filtro2 = new FiltroFechaDeSalida(LocalDate.of(2023, 11, 15));
-        List<Filtro> filtros= new ArrayList<Filtro>();
+        List<Filtro> filtros = new ArrayList<Filtro>();
         filtros.add(filtro1);
         filtros.add(filtro2);
         FiltroAnd filtro = new FiltroAnd(filtros);
+        List<Viaje> viajesFiltrados = terminal1.obtenerViajesPorFiltro(filtro);
+
+        assertEquals(1, viajesFiltrados.size());
+        assertEquals(viaje1, viajesFiltrados.getFirst());
+     }
+	 
+	 @Test
+	 public void testFiltroOr() {
+        FiltroFechaDeLlegada filtro1 = new FiltroFechaDeLlegada(LocalDate.of(2023, 12, 1));
+        FiltroFechaDeSalida filtro2 = new FiltroFechaDeSalida(LocalDate.of(2023, 11, 15));
+        List<Filtro> filtros = new ArrayList<Filtro>();
+        filtros.add(filtro1);
+        filtros.add(filtro2);
+        FiltroOr filtro = new FiltroOr(filtros);
         List<Viaje> viajesFiltrados = terminal1.obtenerViajesPorFiltro(filtro);
 
         assertEquals(1, viajesFiltrados.size());
